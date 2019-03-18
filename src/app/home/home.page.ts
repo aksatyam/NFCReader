bda0e85aeccc131b721d411fbb12fe15c587dda9
aksatytam <ashishsatyam.kmr@gmail.com>
2019-03-18 16:15:43 +0530

    implementation done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { Component } from '@angular/core';
 import { NFC, Ndef } from '@ionic-native/nfc/ngx';
 
@@ -9,8 +9,15 @@ import { NFC, Ndef } from '@ionic-native/nfc/ngx';
 })
 export class HomePage {
 
-  constructor(private nfc: NFC, private ndef: Ndef, public pltf: Platform) {
+  constructor(private nfc: NFC, private ndef: Ndef, public pltf: Platform, public alertCtrl: AlertController) {
     console.log('Home Page');
+    this.nfc.enabled().then(enabled => {
+      console.log(`NFC Enable: ${enabled}`);
+    })
+      .catch(err => {
+        console.log(err);
+        this.presentAlert();
+      });
   }
 
   onStartScan() {
@@ -18,21 +25,35 @@ export class HomePage {
     this.pltf.ready().then(() => {
       console.log('Platform is Ready');
       this.nfc.enabled().then(enabled => {
-        console.log(enabled);
-        this.nfc.addNdefListener(onSuccess => {
-          console.log('successfully attached ndef listener');
-          console.log(onSuccess);
-        }, onFailure => {
-          console.log(onFailure);
+        console.log(`NFC Enable: ${enabled}`);
+        this.nfc.addTagDiscoveredListener(success => {
+          console.log(`On Scan Failure: ${success}`);
+        }, failure => {
+          console.log(`On Scan Failure: ${failure}`);
         });
       })
-      .catch(err => {
-        console.log(err);
-        this.nfc.showSettings();
-      });
+        .catch(err => {
+          console.log(`NFC Disable: ${err}`);
+          this.presentAlert();
+        });
     })
-    .catch(err => {
-      console.log(`Platfor error: ${err}`);
+      .catch(err => {
+        console.log(`Platform error: ${err}`);
+      });
+  }
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'NFC Warning',
+      message: 'Please, enable NFC to scan the NFC-Card',
+      backdropDismiss: false,
+      buttons: [{
+        text: 'OK',
+        handler: () => {
+          this.nfc.showSettings();
+        }
+      }]
     });
+    return await alert.present();
   }
 }
